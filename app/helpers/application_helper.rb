866eca57185ad4dbe84f637883a0d983d0f826ca
Thomas Falcone <thomas.falcone06@gmail.com>
2019-03-11 00:48:57 +0100

    Add(Helper for title) Fix(Missing title edit page)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(title = '') 
+    main_title = "MenexaTech"
+    if title.empty?
+      main_title
+    else 
+      title + " | " + main_title
+    end
+  end
 end
